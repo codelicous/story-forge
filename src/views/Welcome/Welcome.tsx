882a7f33fc191cb950ccf813c7278055d748b18c
codelicous <ktomkt86@gmail.com>
2025-05-01 12:59:15 +0300

    fixed add player behaviour

--- src/views/Welcome/Welcome.tsx
+++ src/views/Welcome/Welcome.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useGame } from '@contexts/game.context';
 import { MAX_PLAYERS, PlayerColorBank, quickPlayPlayers } from '@components/app/consts';
@@ -10,54 +10,59 @@ const playerColors = Object.values(PlayerColorBank);
 export const Welcome = (): React.JSX.Element => {
     const navigate = useNavigate();
     const { config: { players, openerCategory }, setConfig } = useGame();
-    const inputRef = React.useRef<HTMLInputElement>(null);
+    const [currentPlayerName, setCurrentPlayerName] = useState("");
     const onCategoryChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
         setConfig((prevState) => ({ ...prevState, openerCategory: event.target.value as Game['openerCategory'] }));
     }, [setConfig]);
     const isGameValid = useCallback(() => players?.length > 1 && players.every(player => player.name.trim().length > 2), [players]);
-
+    const playerNameValid = useCallback(() => currentPlayerName.length > 2, [currentPlayerName]);
     const onPlayClick = useCallback(() => {
         navigate('/game');
     }, [players, navigate]);
-
-
     const onQuickPlayClick = useCallback(async () => {
-
         setConfig((prevState) => ({ ...prevState, players: quickPlayPlayers }));
         navigate('/quickplay');
-    },[navigate, setConfig]);
+    }, [navigate, setConfig]);
 
     const addPlayer = useCallback(() => {
-        const value = inputRef.current?.value;
-        if (value) {
+        if (currentPlayerName) {
             setConfig((prevState) => ({
                 ...prevState,
                 players: [...prevState.players, {
-                    name: value,
+                    name: currentPlayerName,
                     id: prevState.players.length + 1,
                     color: playerColors[prevState.players.length + 1]
                 }]
             }));
+            setCurrentPlayerName('');
         }
-    }, [setConfig]);
+    }, [setConfig, currentPlayerName]);
 
     return (
         <div className="form-control max-w-xs mx-auto gap-4">
             <h2>Welcome to StoryForge!</h2>
             <label>
-                <p>Select a category</p>
+                <p>Select a story category</p>
                 { categories.map((category) => (
                     <label key={ category } className="label cursor-pointer">
-                        <span className="label-text">{ category }</span>
+                        <span className="label-text capitalize">{ category }</span>
                         <input checked={ openerCategory === category } onChange={ onCategoryChange } type="radio"
                                name="category" value={ category } className="radio checked:bg-black-500"/>
                     </label>
                 )) }
             </label>
+
             <label className="input input-bordered flex items-center gap-2">
-                <input ref={ inputRef } className="grow" placeholder="Name" type="text"/>
-                { players.length <= MAX_PLAYERS && <kbd className="kbd kbd-sm" onClick={ addPlayer }>+</kbd> }
+
+                <input value={ currentPlayerName } onChange={ event => setCurrentPlayerName(event.target.value) }
+                       className="grow" placeholder="Name" type="text"/>
+                { players.length <= MAX_PLAYERS &&
+                    <button
+                        disabled={ !playerNameValid() }
+                        className="btn btn-primary"
+                        onClick={ addPlayer }>Add Player</button> }
             </label>
+
             {
                 players.map((player) => (
                     <div key={ player.id } className="flex items-center gap-2">
